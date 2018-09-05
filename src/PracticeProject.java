a3d7cf1c02805f682b0833bdec734e1172a47a1f
Samuel Simonds <sSamuelSimonds@yccc.edu>
2018-09-05 19:18:01 -0400

    first commit

---
+++ src/PracticeProject.java
@@ -0,0 +1,17 @@
+package PracticeProject;
+
+public class PracticeProject 
+{
+
+	
+	private String name = "Sam";
+	
+
+	public static void main(String[] args)
+	{
+
+	System.out.println("I have no clue what I'm doing");
+	System.out.println("Yay I made a second one");
+
+	}
+}
---
+++ src/TheBestClass.java
@@ -0,0 +1,17 @@
+
+public class TheBestClass {
+	
+	//An object is 2 thing.
+	// 1. Actions. Methods.
+	// 2. Attributes. Variables
+	private String name = "Mike";
+	
+	// main method. Starts the program.
+	// Method Signature - What makes this method unique. Defines the method.
+	public static void main(String[] args) 
+	{
+		// Prints to the console.
+		System.out.println("Hello World");
+		System.out.println("My first java ap!!");
+	}
+}
